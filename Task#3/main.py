dadd3589919e8b2f95e5cc26cea2797c62b6551d
AndreyVanyuck <avanyuck@gmail.com>
2020-11-25 23:18:14 +0300

    fix task_3

--- Task#3/main.py
+++ Task#3/main.py
@@ -1,18 +1,27 @@
-from multiprocessing import Pool
+from threading import Lock
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
+
+@dataclass
+class A:
+    val : int
 
 
 def function(arg):
-    a = 0
-    for _ in range(arg):
-        a += 1
-    return a
+    for _ in range(arg[0]):
+        arg[1].acquire()
+        arg[2].val += 1
+        arg[1].release()
 
 
 def main():
-    args = [1000000, 1000000, 1000000, 1000000, 1000000]
-    pool = Pool(processes=5)
-    result = pool.map(function, args)
-    print("----------------------", sum(result)) 
- 
+    lock = Lock()
+    a = A(0)
+    args = [ [1000000, lock, a] ] * 5
+    with ThreadPoolExecutor() as executor:
+        executor.map(function, args)
+
+    print("----------------------", a.val)
+
 
 main()
