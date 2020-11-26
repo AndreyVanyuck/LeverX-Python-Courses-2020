c3a49453c080dbff928d1425f9f1513477dec312
AndreyVanyuck <avanyuck@gmail.com>
2020-11-26 21:42:20 +0200

    changed parser logic

--- Task#2/my_parser.py
+++ Task#2/my_parser.py
@@ -3,31 +3,35 @@ from collections import namedtuple
 
 
 class Parser:
-    def __init__(self, version):
-        self.version = version
+   
+    def parse(self, vers):
+        split_version = [0, 0, 0, "", ""]   #['major', 'minor', 'patch_int', 'patch_str', 'pre_release_vers']
 
-    def parse(self):
-        version_without_metadata = self.version.split('+')
+        version_without_metadata = vers.split('+')
         vers = version_without_metadata[0].split('.', maxsplit=2)    
-        major = int(vers[0])
-        minor = int(vers[1])
-
-        Patch = namedtuple('Patch',['patch_int', 'patch_str']) 
-        patch = vers[2].split("-", maxsplit=1)[0]
+        
+        split_version[0] = int(vers[0]) 
+        split_version[1] = int(vers[1])
+        
+        try:
+            split_version[4] = vers[2].split("-", maxsplit=1)[1]
+        except IndexError:
+            split_version[4] = "z"
+        try: 
+            patch = vers[2].split("-", maxsplit=1)[0]
+        except IndexError:
+            return split_version
+    
+        
         if (patch.isdigit()):
-            patch = Patch(int(patch), None)
+            split_version[2] = int(patch)
         else:
             index = 0
             for ind, val in enumerate(patch):
                 if (val not in digits):
                     index = ind
                     break
-            patch_int = int(patch[:index])
-            patch_str = patch[index:]
-            patch = Patch(patch_int, patch_str)
-                    
-        try:
-            pre_release_vers = vers[2].split("-", maxsplit=1)[1]
-        except IndexError:
-            pre_release_vers = None
-        return major, minor, patch, pre_release_vers
+            split_version[2] = int(patch[:index])
+            split_version[3] = patch[index:]
+                
+        return split_version
