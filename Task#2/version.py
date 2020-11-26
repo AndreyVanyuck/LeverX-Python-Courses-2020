5f0e7048cd401e3bd424320a96acfca713fae541
AndreyVanyuck <avanyuck@gmail.com>
2020-11-26 21:44:45 +0200

    removed many elif statements

--- Task#2/version.py
+++ Task#2/version.py
@@ -5,42 +5,20 @@ from my_parser import Parser
 @total_ordering
 class Version:
     def __init__(self, version):
-        self.major, self. minor, self.patch, self.pre_release_vers = Parser(version).parse()
+        self.split_version = Parser().parse(version)
+
 
     def __eq__(self, other):
-        return (self.major == other.major
-               and self.minor == other.minor 
-               and self.patch.patch_int == other.patch.patch_int
-               and self.patch.patch_str == other.patch.patch_str 
-               and self.pre_release_vers == other.pre_release_vers)
+        return self.split_version == other.split_version
+
             
 
     def __lt__(self, other):
-        if (self.major < other.major):
-            return True
-        elif self.major > other.major:
-            return False
-        elif self.minor < other.minor:
-            return True
-        elif self.minor > other.minor:
-            return False
-        elif self.patch.patch_int < other.patch.patch_int:
-            return True
-        elif self.patch.patch_int > other.patch.patch_int:
-            return False
-        elif self.patch.patch_str is None and other.patch.patch_str is not None:
-            return True
-        elif self.patch.patch_str is None  and other.patch.patch_str is None:
-            if other.pre_release_vers is None:
-                return True
-            if self.pre_release_vers is None:
-                return False
-            if self.pre_release_vers < other.pre_release_vers:
+        for i, j in zip(self.split_version, other.split_version):
+            if i == j:
+                continue
+            if i < j:
                 return True
             else:
-                return False
-        elif self.patch.patch_str is not None  and other.patch.patch_str is not None:
-            if self.patch.patch_str < other.patch.patch_str:
-                return True
-            else:
-                return False
+                return False 
+        return False   
